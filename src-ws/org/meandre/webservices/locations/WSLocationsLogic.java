833aec1dd8890043cebfa1de89af2f9ccf64382e
Xavier Llora <xllora@illinois.edu>
2008-02-02 05:52:17 +0000

    MDR More big refactoring

---
+++ src-ws/org/meandre/webservices/locations/WSLocationsLogic.java
@@ -0,0 +1,260 @@
+package org.meandre.webservices.locations;
+
+import org.meandre.webservices.WSCoreBootstrapper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.meandre.core.store.Store;
+import org.meandre.core.store.repository.QueryableRepository;
+import org.meandre.core.store.repository.RepositoryImpl;
+import org.meandre.core.store.system.SystemStore;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+/** This class groups all the basic logic required to maintain the locations
+ * for the Meandre webservices interface.
+ * 
+ * @author Xavier Llor&agrave;
+ *
+ */
+public class WSLocationsLogic {
+
+	/** The logger for the bootstrapper */
+    protected static Logger log = null;
+
+    // Initializing the logger and its handlers
+    static {
+        log = Logger.getLogger(WSCoreBootstrapper.class.getName());
+        log.setLevel(Level.CONFIG);
+        log.addHandler(WSCoreBootstrapper.handler);
+    }
+	
+    /** Returns a JSON object for the given location.
+     * 
+     * @param sLocation The location
+     * @param sDescription The description
+     * @return The JSON object
+     * @throws JSONException The location could not be created
+     */
+	static JSONObject locationDescriptionToJSON ( String sLocation, String sDescription ) throws JSONException {
+		JSONObject jo = new JSONObject();
+
+		try {
+			jo.put("location", sLocation);
+			jo.put("description", sDescription);
+		} catch (JSONException e) {
+			log.warning("Could not create a JSON location description for "+sLocation+", "+sDescription);
+			throw e;
+		}
+		
+		return jo;		
+	}
+	/** Prepares the results objects
+	 * 
+	 * @param sUser The user requesting the listing
+	 * @return The array of results
+	 * @throws IOException
+	 * @throws JSONException 
+	 */
+	static JSONObject listLocationsAsJSONObject ( String sUser )
+			throws IOException, JSONException {
+		
+		SystemStore ss = Store.getSystemStore(sUser);
+		
+ 	    Set<Hashtable<String, String>> setProps = ss.getProperty(SystemStore.REPOSITORY_LOCATION);
+		
+		JSONObject joRes = new JSONObject();
+		JSONArray ja = new JSONArray();
+		
+		for ( Hashtable<String, String> ht:setProps ) 
+			ja.put(locationDescriptionToJSON(ht.get("value"), ht.get("description")));
+					
+		joRes.put("location_information",ja);
+		
+		return joRes;
+	}
+
+	/** This method adds a location to the user repository. Also checks that is
+	 * a valid description of it.
+	 * 
+	 * @param The user adding the location
+	 * @param sLocation
+	 * @param sDescription
+	 * @return True if the location could be successfully added
+	 */
+	static boolean addLocation(String sUser, String sLocation, String sDescription) {
+		
+		boolean bRes = true;
+		
+		// Retrieve system store
+		SystemStore ss = Store.getSystemStore(sUser);
+	    
+		if ( !isAlreadyAUsedLocation(ss,sLocation) ) {
+			//
+			// New location
+			//
+			try {
+					
+				URL url = new URL(sLocation);
+				Model modelTmp = ModelFactory.createDefaultModel();
+					
+				modelTmp.setNsPrefix("", "http://www.meandre.org/ontology/");
+				modelTmp.setNsPrefix("xsd", "http://www.w3.org/2001/XMLSchema#");
+					modelTmp.setNsPrefix("rdfs","http://www.w3.org/2000/01/rdf-schema#");
+				modelTmp.setNsPrefix("dc","http://purl.org/dc/elements/1.1/");
+	    
+				//
+				// Read the location and check its consistency
+				//
+				if ( sLocation.endsWith(".ttl"))
+					modelTmp.read(url.openStream(),null,"TTL");
+				else if ( sLocation.endsWith(".nt"))
+					modelTmp.read(url.openStream(),null,"N-TRIPLE");
+				else
+					modelTmp.read(url.openStream(),null);
+				
+				//
+				// Test the location
+				//
+				new RepositoryImpl(modelTmp);
+				
+				//
+				// If now exception was thrown, add the location to the list
+				// and update the user repository 
+				//
+				ss.setProperty(SystemStore.REPOSITORY_LOCATION, sLocation, sDescription);
+				QueryableRepository qr = Store.getRepositoryStore(sUser);
+				qr.getModel().begin();
+				qr.getModel().add(modelTmp);
+				qr.getModel().commit();
+			}
+			catch ( Exception e ) {
+				log.warning("Failed to add location\n"+e.toString());
+				bRes = false;
+			}
+		}
+		else {
+			//
+			// Existing location (update the description)
+			//
+			ss.removeProperty(SystemStore.REPOSITORY_LOCATION, sLocation);
+			ss.setProperty(SystemStore.REPOSITORY_LOCATION, sLocation, sDescription);
+		}
+
+		
+		return bRes;
+	}
+
+	/** Checks if a location already exist on the users system repository.
+	 * 
+	 * @param ss The users system repository
+	 * @param sLocation The location
+	 * @return True if the location already exist
+	 */
+	private static boolean isAlreadyAUsedLocation(SystemStore ss, String sLocation) {
+		boolean bExist = false;
+		Set<Hashtable<String, String>> setProps = ss.getProperty(SystemStore.REPOSITORY_LOCATION);
+		
+		for ( Hashtable<String, String> ht:setProps )
+			if ( ht.get("value").equals(sLocation) ) {
+				bExist = true;
+				break;
+			}
+
+		return bExist;
+	}
+	
+	/** Removes a location from the repository.
+	 * 
+	 * @param sUser The system store user
+	 * @param sLocation The location to remove
+	 * @return True if the location could be successfully removed
+	 */
+	static boolean removeLocation(String sUser, String sLocation) {
+		boolean bRes = true;
+		
+		// Retrieve system store
+		SystemStore ss = Store.getSystemStore(sUser);
+	    
+		if ( !isAlreadyAUsedLocation(ss, sLocation)) {
+			//
+			// Location does not exist
+			//
+			bRes = false;
+		}
+		else {
+			//
+			// Location does exist
+			//
+			
+			// 
+			// Remove the location form the system properties
+			//
+			ss.removeProperty(SystemStore.REPOSITORY_LOCATION, sLocation);
+			
+			//
+			// Regenerate the users repository
+			//
+			QueryableRepository qr = Store.getRepositoryStore(sUser);
+			
+			// Cleaning the user repository entries
+			Model mod = qr.getModel();
+			mod.begin();
+			mod.removeAll();
+			mod.commit();
+			
+			// Regenerating the user repository entries
+			Set<Hashtable<String, String>> setProps = ss.getProperty(SystemStore.REPOSITORY_LOCATION);
+			for ( Hashtable<String, String> ht:setProps ) {
+				String sLoc = ht.get("value");
+				try {
+					URL url = new URL(sLoc);
+					Model modelTmp = ModelFactory.createDefaultModel();
+						
+					modelTmp.setNsPrefix("", "http://www.meandre.org/ontology/");
+					modelTmp.setNsPrefix("xsd", "http://www.w3.org/2001/XMLSchema#");
+						modelTmp.setNsPrefix("rdfs","http://www.w3.org/2000/01/rdf-schema#");
+					modelTmp.setNsPrefix("dc","http://purl.org/dc/elements/1.1/");
+		    
+					//
+					// Read the location and check its consistency
+					//
+					if ( url.toString().endsWith(".ttl"))
+						modelTmp.read(url.openStream(),null,"TTL");
+					else if ( url.toString().endsWith(".nt"))
+						modelTmp.read(url.openStream(),null,"N-TRIPLE");
+					else
+						modelTmp.read(url.openStream(),null);
+					
+					new RepositoryImpl(modelTmp);
+					
+					//
+					// If now exception was thrown, add the location to the list
+					// and update the user repository 
+					//
+					mod.begin();
+					mod.add(modelTmp);
+					mod.commit();
+				}
+				catch ( Exception e ) {
+					log.warning("Failed to load location\n"+e.toString());
+					bRes = false;
+				}
+			}
+		}
+		
+		return bRes;
+	}
+
+
+
+}
---
+++ src-ws/org/meandre/webservices/pub/WSPublic.java
@@ -0,0 +1,146 @@
+package org.meandre.webservices.pub;
+
+import org.meandre.webservices.WSCoreBootstrapper;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.meandre.core.repository.test.RepositoryGenerator;
+import org.meandre.core.store.Store;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.vocabulary.DC;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import com.hp.hpl.jena.vocabulary.XSD;
+
+/** A basic handler to display public Meandre information.
+ * 
+ * @author Xavier Llor&agrave;
+ *
+ */
+public class WSPublic extends HttpServlet {
+
+    /** A default serial ID */
+	private static final long serialVersionUID = 1L;
+	
+	/** The logger for the bootstrapper */
+    protected static Logger log = null;
+
+    // Initializing the logger and its handlers
+    static {
+        log = Logger.getLogger(WSCoreBootstrapper.class.getName());
+        log.setLevel(Level.CONFIG);
+        log.addHandler(WSCoreBootstrapper.handler);
+    }
+	
+	/**
+	 * Dispatches web requests for Meandre web services.
+	 * 
+	 * @param sTarget
+	 *            The target path
+	 * @param request
+	 *            The request object
+	 * @param response
+	 *            The response object
+	 * @param dispath
+	 *            The dispatch flag
+	 * @throws IOException
+	 *             An IO exception arised when processing the request
+	 * @throws ServletException
+	 *             The servlet could not complete the request
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
+    	
+    	String [] saParts = new URL(request.getRequestURL().toString()).getPath().split("\\.");
+   		String sTarget = saParts[0];
+		String sExtension = "";
+
+    	if ( saParts.length==2 ) {
+    		sExtension = saParts[1];
+    	}
+    	
+    	if ( sTarget.endsWith("/repository") ) {
+			if ( sExtension.endsWith("rdf") ) {
+				dumpRepository(request,response,Store.getPublicRepositoryStore(),"RDF/XML-ABBREV");
+			}
+			else if ( sExtension.endsWith("ttl") ) {
+				dumpRepository(request,response,Store.getPublicRepositoryStore(),"TTL");
+			}
+			else if ( sExtension.endsWith("nt") ) {
+				dumpRepository(request,response,Store.getPublicRepositoryStore(),"N-TRIPLE");
+			}
+			else  {
+				// 
+				// Invalid request found
+				//
+				log.info("Uknown public service requested "+sTarget);
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			}
+    	}
+    	else if ( sTarget.endsWith("/demo_repository") ) {
+			if ( sExtension.endsWith("rdf") ) {
+				dumpRepository(request,response,RepositoryGenerator.testHelloWorld(),"RDF/XML-ABBREV");
+			}
+			else if ( sExtension.endsWith("ttl") ) {
+				dumpRepository(request,response,RepositoryGenerator.testHelloWorld(),"TTL");
+			}
+			else if ( sExtension.endsWith("nt") ) {
+				dumpRepository(request,response,RepositoryGenerator.testHelloWorld(),"N-TRIPLE");
+			}
+			else  {
+				// 
+				// Invalid request found
+				//
+				log.info("Uknown public service requested "+sTarget);
+				response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			}
+    	}
+    	else {
+    		// 
+			// Invalid request found
+			//
+			log.info("Uknown public service requested "+sTarget);
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    	}
+		
+		
+	}
+
+    /** Dumps the public repository.
+     * 
+     * @param request The request object
+     * @param response The response object
+     * @param sFormat The format
+     * @throws IOException Problem arised when dumping the output
+     */
+	private void dumpRepository(HttpServletRequest request,
+			HttpServletResponse response, Model model, String sFormat) throws IOException {
+		
+		model.setNsPrefix("meandre", Store.MEANDRE_ONTOLOGY_BASE_URL );
+		model.setNsPrefix("xsd", XSD.getURI());
+		model.setNsPrefix("rdf", RDF.getURI());
+		model.setNsPrefix("rdfs",RDFS.getURI());
+		model.setNsPrefix("dc",DC.getURI());
+
+		response.setStatus(HttpServletResponse.SC_OK);
+		
+		if ( sFormat.equals("RDF/XML-ABBREV") ) 
+			response.setContentType("application/xml");
+		else
+			response.setContentType("text/plain");
+		
+		model.write(response.getOutputStream(),sFormat);
+		
+	}
+
+	
+}
