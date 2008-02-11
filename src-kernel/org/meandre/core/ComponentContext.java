22f53eafef7206f32ba6c18a18f030b9abb9a00c
Xavier Llora <xllora@illinois.edu>
2008-02-11 16:04:13 +0000

    MDR Committing the change in the executable component interface signature to have access to the properties of the component during the initializations and disposing phases. THIS CHANGE BREAKS ALL THE CURRENT COMPONENTS AND NEED TO BE ADAPTED IN THEIR SIGNATURES. (Recommitted to shuffle a few methods)

--- src-kernel/org/meandre/core/ComponentContext.java
+++ src-kernel/org/meandre/core/ComponentContext.java
@@ -1,9 +1,5 @@
 package org.meandre.core;
 
-import java.net.URL;
-
-import org.meandre.webui.WebUIFragmentCallback;
-
 /** The component context contains methods to access the context environment
  * during the execution of a component.
  *
@@ -36,43 +32,4 @@ extends ComponentContextProperties {
 	 */
 	public boolean isInputAvailable ( String sInputBuffer ) throws ComponentContextException;
 
-	/** Starts the web-based user interface given the proper implementation of the
-	 * webui callback to deal with user action on the client.
-	 *
-	 * @param wuiCall The webui call back object
-	 */
-	public void startWebUIFragment ( WebUIFragmentCallback wuiCall ) ;
-
-	/** Stops the web-based user interface.
-	 *
-	 * @param wuiCall The webui call back object
-	 */
-	public void stopWebUIFragment (WebUIFragmentCallback wuiCall);
-
-	/** Stops all the web-based user interface created by this module.
-	 *
-	 */
-	public void stopAllWebUIFragments ();
-
-	/** Get the webUI URL.
-	 *
-	 * @param bName True if the url needs to be build using the name.
-	 *              False build the URL using the IP address.
-	 * @return The webUI URL
-	 * @throws ComponentContextException Problem recovering the IP
-	 *
-	 *
-	 */
-	public URL getWebUIUrl ( boolean bName ) throws ComponentContextException;
-
-	/** Returns the unique ID of the executable instance for the current flow.
-	 *
-	 * @return The unique execution instance ID
-	 */
-	public String getExecutionInstanceID ();
-	/** Return the unique ID of the executable flow.
-	 *
-	 * @return
-	 */
-	public String getFlowExecutionInstanceID();
 }
--- src-kernel/org/meandre/core/ComponentContextProperties.java
+++ src-kernel/org/meandre/core/ComponentContextProperties.java
@@ -1,6 +1,9 @@
 package org.meandre.core;
 
+import java.net.URL;
 import java.util.logging.Logger;
+
+import org.meandre.webui.WebUIFragmentCallback;
 
 /** The component context contains methods to access the context environment
  * properties of a component.
@@ -42,4 +45,45 @@ public interface ComponentContextProperties {
 	 * @return The logger object
 	 */
 	public Logger getLogger();
+	
+
+	/** Starts the web-based user interface given the proper implementation of the
+	 * webui callback to deal with user action on the client.
+	 *
+	 * @param wuiCall The webui call back object
+	 */
+	public void startWebUIFragment ( WebUIFragmentCallback wuiCall ) ;
+
+	/** Stops the web-based user interface.
+	 *
+	 * @param wuiCall The webui call back object
+	 */
+	public void stopWebUIFragment (WebUIFragmentCallback wuiCall);
+
+	/** Stops all the web-based user interface created by this module.
+	 *
+	 */
+	public void stopAllWebUIFragments ();
+
+	/** Get the webUI URL.
+	 *
+	 * @param bName True if the url needs to be build using the name.
+	 *              False build the URL using the IP address.
+	 * @return The webUI URL
+	 * @throws ComponentContextException Problem recovering the IP
+	 *
+	 *
+	 */
+	public URL getWebUIUrl ( boolean bName ) throws ComponentContextException;
+
+	/** Returns the unique ID of the executable instance for the current flow.
+	 *
+	 * @return The unique execution instance ID
+	 */
+	public String getExecutionInstanceID ();
+	/** Return the unique ID of the executable flow.
+	 *
+	 * @return
+	 */
+	public String getFlowExecutionInstanceID();
 }
