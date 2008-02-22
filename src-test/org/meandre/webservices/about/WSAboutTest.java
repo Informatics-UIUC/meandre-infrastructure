f4379bb94a26c05814c27732058341422ecddc49
Amit Kumar <amitku@illinois.edu>
2008-02-22 08:12:29 +0000

    Added httpUnit libraries in preparation for the [MDR-19] http://leo2vm02.ncsa.uiuc.edu:8080/browse/MDR-19

---
+++ src-test/org/meandre/webservices/about/WSAboutTest.java
@@ -0,0 +1,33 @@
+/*
+ * @(#) WSAboutTest.java @VERSION@
+ * 
+ * Copyright (c) 2008+ Amit Kumar
+ * 
+ * The software is released under ASL 2.0, Please
+ * read License.txt
+ *
+ */
+package org.meandre.webservices.about;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WSAboutTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
