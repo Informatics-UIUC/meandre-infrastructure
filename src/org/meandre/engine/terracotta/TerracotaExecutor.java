7c1a1fee076c939ef83126a609176fabe0f9a83e
Xavier Llora <xllora@illinois.edu>
2008-04-21 20:31:36 +0000

    MDRTRC-1 Initial take to the firing event class

---
+++ src/org/meandre/engine/terracotta/TerracotaExecutor.java
@@ -0,0 +1,17 @@
+package org.meandre.engine.terracotta;
+
+/** The basic class that implements the Terracotta execution engine.
+ * 
+ * @author Xavier Llor&agrave;
+ *
+ */
+public class TerracotaExecutor {
+
+	/** The Terracotta executor entry point.
+	 * 
+	 * @param The commandline utilities
+	 */
+	public static void main ( String [] sArgs ) {
+		
+	}
+}
