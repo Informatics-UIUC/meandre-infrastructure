927548331618bc1a0a3a9f2bdc300157540aa6b8
Peter Groves <pgroves@illinois.edu>
2008-04-25 16:52:19 +0000

    first checkin of MeandreClient

---
+++ src-ws/org/meandre/client/TransmissionException.java
@@ -0,0 +1,28 @@
+package org.meandre.client;
+
+public class TransmissionException extends Exception {
+
+    private static final long serialVersionUID = 6312322678217327472L;
+
+    public TransmissionException() {
+        super();
+        
+    }
+
+    public TransmissionException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+        
+    }
+
+    public TransmissionException(String arg0) {
+        super(arg0);
+        
+    }
+
+    public TransmissionException(Throwable arg0) {
+        super(arg0);
+        
+    }
+
+    
+}
