d6ca0017eef9cc5dc2f087595f5fc4726b1f7a1b
Amit Kumar <amitku@illinois.edu>
2008-05-19 05:53:26 +0000

    Updated test.

--- src-tests/ws/org/meandre/webservices/about/WSExecuteTest.java
+++ src-tests/ws/org/meandre/webservices/about/WSExecuteTest.java
@@ -9,8 +9,19 @@
  */
 package org.meandre.webservices.about;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.junit.Test;
 import org.meandre.webservices.BaseServletTest;
+import org.xml.sax.SAXException;
+
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
 
 public class WSExecuteTest extends BaseServletTest {
 
@@ -22,12 +33,56 @@ public class WSExecuteTest extends BaseServletTest {
 
 	String LIST_FLOW_URL_JSON="{\"meandre_running_flows\"";
 	String LIST_FLOW_URL_XML ="<meandre_execution>";
+	String LIST_FLOW_URL_TXT =" ";
 
+	
 
 
 	@Test
-	public void testMe(){
-
+	public void testFlowJson(){
+		String format = "json";
+		WebRequest request = new GetMethodWebRequest(getMeandreHostUrl()
+				+ LIST_FLOW_URL + "." + format);
+		WebResponse response = null;
+		try {
+			response = getWebConversation().getResponse(request);
+		} catch (MalformedURLException e) {
+			fail(e.toString());
+		} catch (IOException e) {
+			fail(e.toString());
+		} catch (SAXException e) {
+			fail(e.toString());
+		}
+		try {
+			assertEquals(response.getText().startsWith(
+					LIST_FLOW_URL_JSON), Boolean.TRUE);
+		} catch (IOException e) {
+			fail(e.toString());
+		}
 	}
 
+	@Test
+	public void testFlowXML(){
+		String format = "xml";
+		WebRequest request = new GetMethodWebRequest(getMeandreHostUrl()
+				+ LIST_FLOW_URL + "." + format);
+		WebResponse response = null;
+		try {
+			response = getWebConversation().getResponse(request);
+		} catch (MalformedURLException e) {
+			fail(e.toString());
+		} catch (IOException e) {
+			fail(e.toString());
+		} catch (SAXException e) {
+			fail(e.toString());
+		}
+		try {
+			assertEquals(response.getText().startsWith(
+					LIST_FLOW_URL_XML), Boolean.TRUE);
+		} catch (IOException e) {
+			fail(e.toString());
+		}
+	}
+
+	
 }
