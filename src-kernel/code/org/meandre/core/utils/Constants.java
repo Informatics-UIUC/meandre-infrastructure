d7ffb68fd81d5a29358f9fab1a8e80584da1f061
Amit Kumar <amitku@illinois.edu>
2008-06-27 23:53:03 +0000

    Added qualifier, version and release tag.

--- src-kernel/code/org/meandre/core/utils/Constants.java
+++ src-kernel/code/org/meandre/core/utils/Constants.java
@@ -3,11 +3,13 @@ package org.meandre.core.utils;
 /** This class contains basic global constants used by the engine.
  * 
  * @author Xavier Llor&agrave;
- *
+ * @modified by Amit Kumar: Added Release Tag information and qualifier
  */
 public class Constants {
 
 	/** The engine version */
-	public final static String MEANDRE_VERSION = "1.3vcli (Xuixo)";
+        public final static String MEANDRE_RELEASE_TAG ="Xuixo";
+	public final static String MEANDRE_VERSION = "1.3.1";
+        public final static String MEANDRE_QUALIFIER = "v20080627";
 
 }
