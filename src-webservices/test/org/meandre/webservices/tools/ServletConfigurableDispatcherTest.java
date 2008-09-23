1ac94fee4ebd7ffdcdd8d714a983723a76478af6
Xavier Llora <xllora@illinois.edu>
2008-09-23 20:55:07 +0000

    MDR Fix to the header

--- src-webservices/test/org/meandre/webservices/tools/ServletConfigurableDispatcherTest.java
+++ src-webservices/test/org/meandre/webservices/tools/ServletConfigurableDispatcherTest.java
@@ -1,8 +1,7 @@
 package org.meandre.webservices.tools;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
