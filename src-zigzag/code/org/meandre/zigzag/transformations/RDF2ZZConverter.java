c2a739b4a231aeb7bd1fa7681681b56170731005
Boris Capitanu <capitanu@illinois.edu>
2009-04-15 18:01:44 +0000

    MDR-134: added javadoc comments and license info

--- src-zigzag/code/org/meandre/zigzag/transformations/RDF2ZZConverter.java
+++ src-zigzag/code/org/meandre/zigzag/transformations/RDF2ZZConverter.java
@@ -1,3 +1,45 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright (c) 2008, Board of Trustees-University of Illinois.
+ * All rights reserved.
+ *
+ * Developed by:
+ *
+ * Automated Learning Group
+ * National Center for Supercomputing Applications
+ * http://www.seasr.org
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimers.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimers in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the names of Automated Learning Group, The National Center for
+ *    Supercomputing Applications, or University of Illinois, nor the names of
+ *    its contributors may be used to endorse or promote products derived from
+ *    this Software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ */
+
 package org.meandre.zigzag.transformations;
 
 import java.util.Date;
@@ -15,20 +57,42 @@ import org.meandre.core.repository.QueryableRepository;
 
 import com.hp.hpl.jena.rdf.model.Resource;
 
+/**
+ * Class that generates a ZigZag script based on a flow
+ *
+ * @author Boris Capitanu
+ */
 public class RDF2ZZConverter {
     private static final String NEWLINE = System.getProperty("line.separator");
 
     private final QueryableRepository _repository;
     private String _importSource;
 
+    /**
+     * Constructor
+     *
+     * @param repository The repository containing the flow(s) you want to convert
+     */
     public RDF2ZZConverter(QueryableRepository repository) {
         _repository = repository;
     }
 
+    /**
+     * Sets the location used for importing components
+     *
+     * @param importSource The URL to the file containing the component descriptors
+     */
     public void setImportSource(String importSource) {
         _importSource = importSource;
     }
 
+    /**
+     * Generates a ZigZag script for the specified flow
+     *
+     * @param flowURI The flow URI
+     * @return The ZigZag script
+     * @throws FlowNotFoundException Thrown if the specified flow couldn't be found in the given repository
+     */
     public String generateZZ(String flowURI) throws FlowNotFoundException {
         Map<String, FlowDescription> flows = _repository.getAvailableFlowDescriptionsMap();
         FlowDescription flow = flows.get(flowURI);
@@ -50,6 +114,12 @@ public class RDF2ZZConverter {
         return zzScript.toString();
     }
 
+    /**
+     * Generates the header for the ZigZag script
+     *
+     * @param flow The flow descriptor object
+     * @return The ZigZag header
+     */
     protected String generateZZHeader(FlowDescription flow) {
         StringBuilder sb = new StringBuilder();
         sb.append("#").append(NEWLINE);
@@ -67,10 +137,21 @@ public class RDF2ZZConverter {
         return sb.toString();
     }
 
+    /**
+     * Generates the footer for the ZigZag script
+     *
+     * @param flow The flow descriptor object
+     * @return The ZigZag footer
+     */
     protected String generateZZFooter(FlowDescription flow) {
         return "";
     }
 
+    /**
+     * Generates the 'import' statement for the ZigZag script
+     *
+     * @return The ZigZag import section
+     */
     protected String generateZZImports() {
         StringBuilder sb = new StringBuilder();
         sb.append("#").append(NEWLINE);
@@ -85,44 +166,12 @@ public class RDF2ZZConverter {
         return sb.toString();
     }
 
-    protected Map<Resource, String> createAliasMap(FlowDescription flow) {
-        Map<Resource, String> zzAliases = new HashMap<Resource, String>();
-
-        for (ExecutableComponentInstanceDescription instance : flow.getExecutableComponentInstances()) {
-            Resource resComponent = instance.getExecutableComponent();
-            ExecutableComponentDescription component = _repository.getExecutableComponentDescription(resComponent);
-            String componentAlias = null;
-
-            if ((componentAlias = zzAliases.get(resComponent)) == null) {
-                componentAlias = ((component != null) ? component.getName().replaceAll(" |\t", "_") : getLastPart(resComponent.getURI())).toUpperCase();
-                zzAliases.put(resComponent, componentAlias);
-            }
-        }
-
-        return zzAliases;
-    }
-
-    protected Map<ExecutableComponentInstanceDescription, String> createInstantiationMap(FlowDescription flow) {
-        Map<ExecutableComponentInstanceDescription, String> zzInstances = new HashMap<ExecutableComponentInstanceDescription, String>();
-        Map<ExecutableComponentDescription, Integer> zzInstanceCounter = new HashMap<ExecutableComponentDescription, Integer>();
-
-        for (ExecutableComponentInstanceDescription instance : flow.getExecutableComponentInstances()) {
-            ExecutableComponentDescription component = _repository.getExecutableComponentDescription(instance.getExecutableComponent());
-            String compInstanceName = instance.getName().toLowerCase().replaceAll(" |\t", "_");
-            Integer counter = null;
-
-            if ((counter = zzInstanceCounter.get(component)) != null && zzInstances.containsValue(compInstanceName))
-                compInstanceName += "_" + ++counter;
-            else
-                counter = 1;
-
-            zzInstances.put(instance, compInstanceName);
-            zzInstanceCounter.put(component, counter);
-        }
-
-        return zzInstances;
-    }
-
+    /**
+     * Generates the 'alias' statement(s) for the ZigZag script
+     *
+     * @param aliasMap The component to alias mapping
+     * @return The ZigZag alias section
+     */
     protected String generateZZAliases(Map<Resource, String> aliasMap) {
         StringBuilder sb = new StringBuilder();
         sb.append("#").append(NEWLINE);
@@ -135,6 +184,13 @@ public class RDF2ZZConverter {
         return sb.toString();
     }
 
+    /**
+     * Generates the component instantiations for the ZigZag script
+     *
+     * @param instantiationMap The component instance to ZigZag instance name mapping
+     * @param aliasMap The component to ZigZag alias mapping
+     * @return The ZigZag component instantiations section
+     */
     protected String generateZZInstantiations(
             Map<ExecutableComponentInstanceDescription, String> instantiationMap,
             Map<Resource, String> aliasMap) {
@@ -152,6 +208,12 @@ public class RDF2ZZConverter {
         return sb.toString();
     }
 
+    /**
+     * Generates the component property assignments for the ZigZag script
+     *
+     * @param instantiationMap The component instance to ZigZag instance name mapping
+     * @return The ZigZag component property assignments section
+     */
     protected String generateZZPropertyAssignments(Map<ExecutableComponentInstanceDescription, String> instantiationMap) {
         StringBuilder sb = new StringBuilder();
         sb.append("#").append(NEWLINE);
@@ -171,6 +233,13 @@ public class RDF2ZZConverter {
         return sb.toString();
     }
 
+    /**
+     * Generates the component connections for the ZigZag script
+     *
+     * @param flow The flow descriptor object
+     * @param instantiationMap The component instance to ZigZag instance name mapping
+     * @return The ZigZag component connections section
+     */
     protected String generateZZConnections(FlowDescription flow, Map<ExecutableComponentInstanceDescription, String> instantiationMap) {
         StringBuilder sb = new StringBuilder();
         sb.append("#").append(NEWLINE);
@@ -235,6 +304,7 @@ public class RDF2ZZConverter {
             String[] portAssignments = entry.getValue().split(";");
             sb.append(String.format("%s(", entry.getKey()));
 
+            // change the >N to specify when wrapping should occur: wrap only if there more than N port assignments in this statement
             if (portAssignments.length > 1) {
                 sb.append(NEWLINE);
                 for (int i = 0; i < portAssignments.length; i++)
@@ -248,6 +318,62 @@ public class RDF2ZZConverter {
         return sb.toString();
     }
 
+    /**
+     * Creates the ZigZag aliases for the components used by the flow
+     *
+     * @param flow The flow descriptor object
+     * @return A mapping from components to alias names
+     */
+    protected Map<Resource, String> createAliasMap(FlowDescription flow) {
+        Map<Resource, String> zzAliases = new HashMap<Resource, String>();
+
+        for (ExecutableComponentInstanceDescription instance : flow.getExecutableComponentInstances()) {
+            Resource resComponent = instance.getExecutableComponent();
+            ExecutableComponentDescription component = _repository.getExecutableComponentDescription(resComponent);
+            String componentAlias = null;
+
+            if ((componentAlias = zzAliases.get(resComponent)) == null) {
+                componentAlias = ((component != null) ? component.getName().replaceAll(" |\t", "_") : getLastPart(resComponent.getURI())).toUpperCase();
+                zzAliases.put(resComponent, componentAlias);
+            }
+        }
+
+        return zzAliases;
+    }
+
+    /**
+     * Creates the ZigZag component instantiations
+     *
+     * @param flow The flow descriptor object
+     * @return A mapping from component instances to ZigZag instance names
+     */
+    protected Map<ExecutableComponentInstanceDescription, String> createInstantiationMap(FlowDescription flow) {
+        Map<ExecutableComponentInstanceDescription, String> zzInstances = new HashMap<ExecutableComponentInstanceDescription, String>();
+        Map<ExecutableComponentDescription, Integer> zzInstanceCounter = new HashMap<ExecutableComponentDescription, Integer>();
+
+        for (ExecutableComponentInstanceDescription instance : flow.getExecutableComponentInstances()) {
+            ExecutableComponentDescription component = _repository.getExecutableComponentDescription(instance.getExecutableComponent());
+            String compInstanceName = instance.getName().toLowerCase().replaceAll(" |\t", "_");
+            Integer counter = null;
+
+            if ((counter = zzInstanceCounter.get(component)) != null && zzInstances.containsValue(compInstanceName))
+                compInstanceName += "_" + ++counter;
+            else
+                counter = 1;
+
+            zzInstances.put(instance, compInstanceName);
+            zzInstanceCounter.put(component, counter);
+        }
+
+        return zzInstances;
+    }
+
+    /**
+     * Retrieves the last portion of a URI
+     *
+     * @param uri The URI
+     * @return The last segment of the URI
+     */
     private String getLastPart(String uri) {
         String result = null;
 
