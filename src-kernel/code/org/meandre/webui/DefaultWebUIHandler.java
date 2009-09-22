08b0d02c5717f98cbcce6d132bf70008ec65e6b9
Boris Capitanu <capitanu@illinois.edu>
2009-09-22 21:24:26 +0000

    MDR-169

--- src-kernel/code/org/meandre/webui/DefaultWebUIHandler.java
+++ src-kernel/code/org/meandre/webui/DefaultWebUIHandler.java
@@ -14,24 +14,24 @@ import org.mortbay.jetty.handler.AbstractHandler;
 
 /** This class implements a default WebUI handler instanciated for each
  * WebUI and run when no other handlers are pressent.
- * 
+ *
  * @author Xavier Llor&agrave;
  *
  */
 public class DefaultWebUIHandler extends AbstractHandler {
-	
+
 	/** The parent of this handler */
-	private WebUI webUIParent;
-	
+	private final WebUI webUIParent;
+
 	/** The core configuration */
-	private CoreConfiguration cnf;
+	private final CoreConfiguration cnf;
 
 	/** The date the webUI was started */
-	private Date startedAt;
-	private long startedAtMS;
+	private final Date startedAt;
+	private final long startedAtMS;
 
 	/** Creates the default WebUI handler.
-	 * 
+	 *
 	 * @param webUIParent The parent webUI
 	 * @param cnf The core configuration
 	 */
@@ -41,9 +41,9 @@ public class DefaultWebUIHandler extends AbstractHandler {
 		this.startedAt = new Date();
 		this.startedAtMS = this.startedAt.getTime();
 	}
-	
+
 	/** Implements the default, no web ui available, response.
-	 * 
+	 *
 	 * @param target The target path
 	 * @param request The request object
 	 * @param response The response object
@@ -51,12 +51,12 @@ public class DefaultWebUIHandler extends AbstractHandler {
 	 * @throws IOException An IO exception arised when processing the request
 	 * @throws ServletException The servlet could not complete the request
 	 */
-	public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) 
+	public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch)
 	throws IOException, ServletException {
-		Request base_request = (request instanceof Request) ? 
-				                  (Request) request : 
+		Request base_request = (request instanceof Request) ?
+				                  (Request) request :
 				                  HttpConnection.getCurrentConnection().getRequest();
-		
+
         if (response.isCommitted() || base_request.isHandled())
 		      return;
 		base_request.setHandled(true);
@@ -67,24 +67,26 @@ public class DefaultWebUIHandler extends AbstractHandler {
 				"<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\">\n\n"+
 				"<head>\n"+
 				"<title>Meandre Flow &raquo; Excecution &raquo; WebUI &raquo; "+webUIParent.getFlowExecutionUniqueID()+" </title>\n"+
-				"\t<meta http-equiv=\"content-type\" content=\"text/html; charset=utf8\" />\n"+
-				"\t<meta http-equiv=\"content-language\" content=\"EN\" />\n"+
-				"\t<meta name=\"ROBOTS\" content=\"NOINDEX,NOFOLLOW\"/>\n"+
-				"\t<meta name=\"description\" content=\"Meandre Flow\"/>\n"+
-				"\t<meta http-equiv=\"refresh\" content=\"6\" />\n"+
+				"\t<meta http-equiv=\"content-type\" content=\"text/html; charset=utf8\">\n"+
+				"\t<meta http-equiv=\"content-language\" content=\"EN\">\n"+
+				"\t<meta name=\"ROBOTS\" content=\"NOINDEX,NOFOLLOW\">\n"+
+				"\t<meta http-equiv=\"CACHE-CONTROL\" content=\"NO-CACHE\">\n"+
+				"\t<meta http-equiv=\"Pragma\" content=\"no-cache\">\n" +
+				"\t<meta name=\"description\" content=\"Meandre Flow\">\n"+
+				"\t<meta http-equiv=\"refresh\" content=\"6\">\n"+
 				"\t<style type=\"text/css\">\n"+
 				"\timg {" +
 				"	border: 0px none white; " +
 				"   float: right; " +
 				"\t}\n" +
 				"\tbody { " +
-				"\t   color: #888; " +
+				"\t   color: #777; " +
 				"\t   background: white; " +
 				"\t   font-family: Helvetica, Arial, Verdana; " +
-				"\t   font-size:11px; " +
+				"\t   font-size:14px; " +
 				"\t}\n" +
 				"\t#main p {" +
-				"\t   text-align:center; " +
+				"\t   text-align:left; " +
 				"\t   margin:200px auto 200px auto;" +
 				"}"+
 				"</style>"+
@@ -93,14 +95,15 @@ public class DefaultWebUIHandler extends AbstractHandler {
 				"<div id=\"menu\"> \n" +
 				"<img src=\""+cnf.getAppContext()+"/public/resources/system/logo-meandre.gif\" />\n" +
 				"<div id=\"main\">\n"+
-				"<p>The flow is currently running. Please wait for your results to be displayed here.<br/>No WebUI available at this point of execution.<br/>"+
-				"Started: "+this.startedAt+". <br/>"+
-				"Time elapsed: "+((this.startedAtMS-new Date().getTime())/1000)+" seconds </p>"+
+				"<p>The flow is currently running. Please wait for your results to be displayed here." +
+				"<br/>No WebUI available at this point of execution.<br/>"+
+				"Started: "+this.startedAt+"<br/>"+
+				"Time elapsed: "+((new Date().getTime()-this.startedAtMS))/1000.0+" seconds </p>"+
 				"</div>\n"+
 				"</div>\n"+
 				"</body>\n"+
 				"</html>"
 		);
-	
+
 	}
 }
