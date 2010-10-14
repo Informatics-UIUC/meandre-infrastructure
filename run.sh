e593bfc9c9c91ca869be37eb052aab61deee0c9d
Boris Capitanu <capitanu@illinois.edu>
2010-10-14 15:14:15 +0000

    - added a script that allows for easy launching of Meandre 2.0 - updated server.scala with more appropiate defaults

---
+++ run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+SCALA=scala
+SBT=sbt
+
+MEANDRE2_HOME=`dirname $0`
+MEANDRE2_LOG=$MEANDRE2_HOME/log/meandre.log
+MEANDRE2_STARTUP_SCRIPT=$MEANDRE2_HOME/scripts/server.scala
+
+# DO NOT MODIFY BELOW THIS LINE
+
+MEANDRE2_JAR=$MEANDRE2_HOME/infrastructure/target/scala_2.7.7/infrastructure_2.7.7-2.0.jar
+MEANDRE2_DEPS_DIR=$MEANDRE2_HOME/infrastructure/lib_managed/scala_2.7.7/compile
+
+function buildClasspath() {
+  for dep in $MEANDRE2_DEPS/*.jar
+    do
+      MEANDRE2_CLASSPATH=$MEANDRE2_CLASSPATH:$dep
+    done
+
+  MEANDRE2_CLASSPATH=$MEANDRE2_CLASSPATH:$MEANDRE2_JAR
+  MEANDRE2_CLASSPATH=`echo $MEANDRE2_CLASSPATH | cut -c2-`
+}
+
+if [ ! -d $MEANDRE2_DEPS_DIR ]; then
+  printf "Dependency files are missing... running 'sbt update' to retrieve them...\n"
+  $SBT update 1>>$MEANDRE2_LOG 2>&1 
+  if [ $? -eq 0 ]; then
+    echo "done"
+  else
+    echo "failed"
+    echo "There was a problem running 'sbt update'. Please check $MEANDRE2_LOG for errors."
+    exit 1
+  fi
+fi
+
+if [ ! -f $MEANDRE2_JAR ]; then
+  echo "The Meandre 2.0 package is missing... running 'sbt package' to build it...\n"
+  $SBT package 1>>$MEANDRE2_LOG 2>&1
+  if [ $? -eq 0 ]; then
+    echo "done"
+  else
+    echo "failed"
+    echo "There was a problem running 'sbt package'. Please check $MEANDRE2_LOG for errors."
+    exit 2
+  fi
+fi
+
+buildClasspath
+
+$SCALA -classpath $MEANDRE2_CLASSPATH $MEANDRE2_STARTUP_SCRIPT 1>>$MEANDRE2_LOG 2>&1 &
+
+echo The Meandre 2.0 server is now starting... you can access it at http://$(hostname):1714
--- scripts/server.scala
+++ scripts/server.scala
@@ -1,38 +1,23 @@
 //
 // A basic script to launch a server
 //
+
 import meandre.kernel.Configuration
 import meandre.webservices.api.{MeandreServer}
-//
-// One server
-//
-
-//val TEST_SERVER_PORT = 1714
-//val cnf = Configuration()
-//cnf.EXECUTION_SCALA = "/usr/local/bin/scala"
-//cnf.EXECUTION_SCRIPT = "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/scripts/execution_%s.scala"
-//cnf.EXECUTION_CLASSPATH = "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/out/production/Meandre-Infrastructure"
-//val server = MeandreServer(cnf, "/", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/styling", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/docs")
-//server.go
-
-
-//
-// Three servers
-//
-
 import scala.concurrent.ops.spawn
 
-val TEST_SERVER_PORT = 1714
+val SERVER_PORT = 1714
+val LOCAL_HOST_NAME = java.net.InetAddress.getLocalHost().getHostName()
 
-val (cnf1,cnf2,cnf3) = (
-        Configuration(),
-        Configuration("http","localhost",TEST_SERVER_PORT+1000,"localhost",27017,None),
-        Configuration("http","localhost",TEST_SERVER_PORT+2000,"localhost",27017,None)
+val (cnf1, cnf2, cnf3) = (
+        Configuration("http", LOCAL_HOST_NAME, SERVER_PORT, LOCAL_HOST_NAME, 27017, None),
+        Configuration("http", LOCAL_HOST_NAME, SERVER_PORT + 1000, LOCAL_HOST_NAME, 27017, None),
+        Configuration("http", LOCAL_HOST_NAME, SERVER_PORT + 2000, LOCAL_HOST_NAME, 27017, None)
     )
 
-cnf1.EXECUTION_SCALA = "/usr/local/bin/scala"
-cnf1.EXECUTION_SCRIPT = "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/scripts/execution_%s.scala"
-cnf1.EXECUTION_CLASSPATH = "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/out/production/Meandre-Infrastructure"
+cnf1.EXECUTION_SCALA = "scala"
+cnf1.EXECUTION_SCRIPT = "scripts/execution_%s.scala"
+cnf1.EXECUTION_CLASSPATH = "infrastructure/target/scala_2.7.7/infrastructure_2.7.7-2.0.jar"
 
 cnf2.EXECUTION_SCALA = cnf1.EXECUTION_SCALA
 cnf2.EXECUTION_SCRIPT = cnf1.EXECUTION_SCRIPT
@@ -43,14 +28,16 @@ cnf3.EXECUTION_SCRIPT = cnf1.EXECUTION_SCRIPT
 cnf3.EXECUTION_CLASSPATH = cnf1.EXECUTION_CLASSPATH
 
 
-val server  = MeandreServer(cnf1, "/", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/styling", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/docs")
-val server2 = MeandreServer(cnf2, "/", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/styling", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/docs")
-val server3 = MeandreServer(cnf3, "/", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/styling", "/Users/xavier/Documents/Workspace - IdeaProjects/Meandre-Infrastructure/docs")
+val server  = MeandreServer(cnf1, "/", "infrastructure/src/main/resources/styling/", "docs")
+//val server2 = MeandreServer(cnf2, "/", "infrastructure/src/main/resources/styling/", "docs")
+//val server3 = MeandreServer(cnf3, "/", "infrastructure/src/main/resources/styling/", "docs")
 
-spawn { server.start  }
-spawn { server2.start  }
-server3.start
+//spawn { server2.start  }
+//spawn { server3.start  }
+//server.start
 
-server.join
-server2.join
-server3.join
+server.go
+
+//server.join
+//server2.join
+//server3.join
